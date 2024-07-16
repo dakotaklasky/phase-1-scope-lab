1657ab1d416c1902530dbc80649f1e3dcf2040f3
Dakota <dakota.klasky@gmail.com>
2024-07-16 14:17:44 -0400

    add functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    var bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'val'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'changedval'
+}
